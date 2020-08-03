5ee63450a0b3eecca5bd3ef96451327cc0b161e5
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-03 02:58:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(num)
   return false if num <=1
-  (2..(num-1)).to_a.each do {|n| return false if num%n==0}
-    
+  (2..(num-1)).each do |n|
+    return false if num%n==0
+    end
   true
 end
